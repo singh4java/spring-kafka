d06db1f1f2eb6f5402e74b581e28b9c7f8f88928
Gary Russell <grussell@pivotal.io>
2017-11-02 14:30:17 -0400

    Fix NPE in events when no assigned topics
        java.lang.NullPointerException: null
    	at java.util.ArrayList.<init>(ArrayList.java:177) ~[na:1.8.0_131]
    	at org.springframework.kafka.event.NonResponsiveConsumerEvent.<init>(NonResponsiveConsumerEvent.java:51) ~[spring-kafka-1.3.1.BUILD-SNAPSHOT.jar:na]
    
    __cherry-pick to 2.0.x, 1.3.x__
    
    (cherry picked from commit 21e05ed)

--- spring-kafka/src/main/java/org/springframework/kafka/event/ListenerContainerIdleEvent.java
+++ spring-kafka/src/main/java/org/springframework/kafka/event/ListenerContainerIdleEvent.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 the original author or authors.
+ * Copyright 2016-2017 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -44,7 +44,7 @@ public class ListenerContainerIdleEvent extends KafkaEvent {
 		super(source);
 		this.idleTime = idleTime;
 		this.listenerId = id;
-		this.topicPartitions = new ArrayList<>(topicPartitions);
+		this.topicPartitions = topicPartitions == null ? null : new ArrayList<>(topicPartitions);
 	}
 
 	/**
